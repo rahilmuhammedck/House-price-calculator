824dbc1c93a4a02cbe3104460007eac11c41555e
Muhammed Rahil CK <139740420+rahilmuhammedck@users.noreply.github.com>
2024-05-08 13:23:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ function predictPrice() {
   const garden = document.getElementById('garden').value;
 
   // Perform prediction calculation here (dummy calculation)
-  let predictedPrice = squareFootage * 8000 + bedrooms * 40000 + bathrooms * 24000 - (2024 - yearBuilt) * 80000;
+  let predictedPrice = squareFootage * 100 + bedrooms * 5000 + bathrooms * 3000 - (2024 - yearBuilt) * 1000;
 
   // Adjust price based on car porch and garden availability
   if (carPorch === 'yes') {
-    predictedPrice += 40000; // Add $ for car porch
+    predictedPrice += 5000; // Add $5000 for car porch
   }
   if (garden === 'yes') {
-    predictedPrice += 24000; // Add for garden
+    predictedPrice += 3000; // Add $3000 for garden
   }
 
   // Redirect to result page with predicted price as query parameter
-  window.location.href = `result.html?predictedPrice=₹{predictedPrice}`;
+  window.location.href = `result.html?predictedPrice=${predictedPrice}`;
 }
